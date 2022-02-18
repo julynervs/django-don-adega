307a2d82e68e223b98ba5dbd1216f578929e0e34
Julyelly Roberta <julyellyr@gmail.com>
2022-02-18 14:15:28 -0300

    funções de filtro adicionadas

--- bling/api_db_contasreceber.py
+++ bling/api_db_contasreceber.py
@@ -41,3 +41,88 @@ def coloca_contasreceber_no_banco(retorno_get):
                 return True
             else:
                 return False   
+
+        # cadastro foreign key
+        # # identifica o cliente que fez a conta
+        # # e obtém o mesmo do banco de dados
+        nome_cliente_bling = conta_bling['cliente']['nome']
+        cliente_db = Contato.objects.filter(nome=nome_cliente_bling).order_by('id').first()            
+        conta_db = ContaReceber.objects.create(cliente = cliente_db)
+
+        # cadastros data
+        if chave_existe('dataEmissao'):
+            conta_db = ContaReceber.objects.create(data_emissao = valor_correto_ou_nada('dataEmissao'))   
+            logging.info(f"____data_emissao do pedido {n} cadastrado")
+        else:
+            logging.info(f"____O campo 'data_emissao' NÃO foi cadastrado por não existir no json")
+
+        if chave_existe('vencimento'):
+            conta_db = ContaReceber.objects.create(vencimento = valor_correto_ou_nada('vencimento'))   
+            logging.info(f"____vencimento do pedido {n} cadastrado")
+        else:
+            logging.info(f"____O campo 'vencimento' NÃO foi cadastrado por não existir no json")
+
+        if chave_existe('competencia'):
+            conta_db = ContaReceber.objects.create(competencia = valor_correto_ou_nada('competencia'))   
+            logging.info(f"____competencia do pedido {n} cadastrado")
+        else:
+            logging.info(f"____O campo 'competencia' NÃO foi cadastrado por não existir no json")
+
+        # cadastros float
+        if chave_existe('valor'):
+            conta_db = ContaReceber.objects.create(valor = valor_correto_ou_nada('valor'))   
+            logging.info(f"____valor do pedido {n} cadastrado")
+        else:
+            logging.info(f"____O campo 'valor' NÃO foi cadastrado por não existir no json")
+        if chave_existe('saldo'):
+            conta_db = ContaReceber.objects.create(saldo = valor_correto_ou_nada('saldo'))   
+            logging.info(f"____saldo do pedido {n} cadastrado")
+        else:
+            logging.info(f"____O campo 'saldo' NÃO foi cadastrado por não existir no json")
+        
+        # cadastros int
+        if chave_existe('id'):
+            conta_db = ContaReceber.objects.create(id_bling = valor_correto_ou_nada('id'))   
+            logging.info(f"____id_bling do pedido {n} cadastrado")
+        else:
+            logging.info(f"____O campo 'id_bling' NÃO foi cadastrado por não existir no json")
+        
+        if chave_existe('nroNoBanco'):
+            conta_db = ContaReceber.objects.create(nro_banco = valor_correto_ou_nada('nroNoBanco'))   
+            logging.info(f"____nro_banco do pedido {n} cadastrado")
+        else:
+            logging.info(f"____O campo 'nro_banco' NÃO foi cadastrado por não existir no json")
+
+        # cadastros string
+        if chave_existe('situacao'):
+            conta_db = ContaReceber.objects.create(situacao = valor_correto_ou_nada('situacao'))   
+            logging.info(f"____situacao do pedido {n} cadastrado")
+        else:
+            logging.info(f"____O campo 'situacao' NÃO foi cadastrado por não existir no json")
+
+        if chave_existe('historico'):
+            conta_db = ContaReceber.objects.create(historico = valor_correto_ou_nada('historico'))   
+            logging.info(f"____historico do pedido {n} cadastrado")
+        else:
+            logging.info(f"____O campo 'historico' NÃO foi cadastrado por não existir no json")
+
+        if chave_existe('ocorrencia'):
+            conta_db = ContaReceber.objects.create(ocorrencia = valor_correto_ou_nada('ocorrencia'))   
+            logging.info(f"____ocorrencia do pedido {n} cadastrado")
+        else:
+            logging.info(f"____O campo 'ocorrencia' NÃO foi cadastrado por não existir no json")
+
+        if chave_existe('categoria'):
+            conta_db = ContaReceber.objects.create(categoria = valor_correto_ou_nada('categoria'))   
+            logging.info(f"____categoria do pedido {n} cadastrado")
+        else:
+            logging.info(f"____O campo 'categoria' NÃO foi cadastrado por não existir no json")
+        
+        if chave_existe('vendedor'):
+            conta_db = ContaReceber.objects.create(vendedor = valor_correto_ou_nada('vendedor'))   
+            logging.info(f"____vendedor do pedido {n} cadastrado")
+        else:
+            logging.info(f"____O campo 'vendedor' NÃO foi cadastrado por não existir no json")
+      
+        conta_db.save()
+        print(f"- Conta a receber {n} {conta_bling['historico']} cadastrada")
